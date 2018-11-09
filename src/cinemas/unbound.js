c14adcf50222ebced3f78280f4b01fc53f1f8909
Theepan Thevathasan <nrotechmonk@gmail.com>
2018-11-10 00:15:36 +1100

    create function to return  only the given state's cinemas  + remove duplicates

---
+++ src/cinemas/unbound.js
@@ -0,0 +1,64 @@
+async function allCinemas({ puppeteer, states }) {
+  const cinemaDetails = [];
+  const MODAL_BUTTON_CLOSE_SELECTOR =
+    "body > header > div.fave-wrapper.open > div.fave-modal > span.close";
+  const page = await puppeteer
+    .launch({
+      handleSIGINT: true /*devtools: false,headless: true*/
+    })
+    .then(browser => browser.newPage());
+
+  await page.setViewport({ width: 1366, height: 735 }); //form factor - laptop/PC
+  await page.goto("https://www.eventcinemas.com.au");
+  await page.waitFor(1000);
+
+  //close the popup modal to select cinemas and movies
+  await page.$eval(MODAL_BUTTON_CLOSE_SELECTOR, elem => elem.click());
+  for (const state of states) {
+    const res = await getCinemasfromState(page, state);
+    res.forEach(cin => {
+      //cin.cinemaState = state;
+      cinemaDetails.push(cin);
+    });
+  }
+  await page.close();
+
+  return cinemaDetails;
+}
+
+async function cinemasfromState(page, state) {
+  const CINEMA_STATE_SELECTOR = `div.top-select div.slider span.state[data-state-selector=$[STATE]]`;
+  const CINEMA_SELECTOR = `div[data-state=$[STATE]] div.top-select-option a.eccheckbox`;
+
+  await page.click(CINEMA_STATE_SELECTOR.replace("$[STATE]", state));
+
+  let res = await page.evaluate(
+    (elementPath, state) => {
+      return Array.from(document.querySelectorAll(elementPath))
+        .map(
+          function(cin, index) {
+            let result = {
+              cinemaState: this.state,
+              cinemaId: cin.getAttribute("data-id"),
+              cinemaName: cin.getAttribute("data-name"),
+              cinemaURL: cin.getAttribute("data-url")
+            };
+            return result;
+          },
+          { state }
+        )
+        .reduce((acc, cur) => Object.assign(acc, { [cur.cinemaId]: cur }), {});//filter dupes
+    },
+    CINEMA_SELECTOR.replace("$[STATE]", state),
+    state
+  );
+
+  return res;
+}
+
+/*
+.filter(function(cin, index, cinemas) {
+      return cinemas.indexOf(cin) === index;
+    });
+    */
+export { allCinemas, cinemasfromState };
