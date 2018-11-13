40a49706bd2e6b498c174edef05baf545b39aae4
Theepan Thevathasan <nrotechmonk@gmail.com>
2018-11-13 21:26:11 +1100

    improved loading speed by blocking css and images

---
+++ src/cinemas/all-cinemas.js
@@ -0,0 +1,39 @@
+export default async function allCinemas({
+  puppeteer,
+  cinemasfromState,
+  states
+}) {
+  const cinemaDetails = [];
+  const page = await puppeteer
+    .launch({
+      handleSIGINT: true /*devtools: false,headless: true*/
+    })
+    .then(browser => browser.newPage());
+
+  //block images, CSS to speed up load time
+  await page.setRequestInterception(true);
+
+  page.on("request", req => {
+    if (
+      req.resourceType() === "image" ||
+      req.resourceType() === "stylesheet" ||
+      req.resourceType() === "font"
+    ) {
+      req.abort();
+    } else {
+      req.continue();
+    }
+  });
+
+  await page.goto("https://www.eventcinemas.com.au");
+  await page.waitFor(500);
+
+  //close the popup modal to select cinemas and movies
+  for (const state of states) {
+    const res = await cinemasfromState(page, state);
+    cinemaDetails.push(...res);
+  }
+  await page.close();
+
+  return Promise.resolve(cinemaDetails);
+}
