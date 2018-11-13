99ff6bad94782288ee1ef5c887f0a65574982705
Theepan Thevathasan <nrotechmonk@gmail.com>
2018-11-13 21:27:29 +1100

    moved to glabal mocks

---
+++ src/__mocks__/puppetteer.js
@@ -0,0 +1,22 @@
+//setup mock for page.evaluate with generic return
+const page = {
+  click: jest.fn(() => Promise.resolve()),
+  evaluate: jest.fn((selector, state) => Promise.resolve()),
+  setViewPort: jest.fn(() => Promise.resolve()),
+  goto: jest.fn(() => Promise.resolve()),
+  waitFor: jest.fn(() => Promise.resolve()),
+  $eval: jest.fn(() => Promise.resolve()),
+  close: jest.fn(() => Promise.resolve()),
+  setRequestInterception: jest.fn(() => Promise.resolve()),
+  on: jest.fn(() => Promise.resolve())
+};
+
+const puppeteer = {
+  page: page,
+
+  launch: jest.fn(() =>
+    Promise.resolve({ newPage: jest.fn(() => Promise.resolve(page)) })
+  )
+};
+
+export { puppeteer };
