df616e6f947e45ff3fbbbfe2d238d0e3f7c01663
Theepan Thevathasan <nrotechmonk@gmail.com>
2018-11-26 14:42:37 +1100

    wait for session info before scraping + exit criteria for scraping

--- src/scrape-daily/unbound.js
+++ src/scrape-daily/unbound.js
@@ -20,44 +20,52 @@ Flow :
 */
 
 module.exports = async (
-  {  moviesFn, sessionsFn, urlFn, R = require("ramda") },
-  page, date,
-  cinemas,
+  { moviesFn, sessionsFn, urlFn, R = require("ramda") },
+  page,
+  date,
+  cinemaList,
   movieList,
   limit = 10
 ) => {
   let sessionResults = []; //place holder to
 
-  
 
   let index = 0;
-
-  while (index < cinemas.length) {
+  let  noSessionsCounter = 0 
+//loop until the cinemasList is exhausted and 3 pages with no sessions are attempted (efficiency especially when scraped for few movies rather than more)
+  while (index < cinemaList.length && noSessionsCounter<3) {
     //URL with subset of cinemas in the current loop cycle
     const url = await urlFn(date, movieList, {
-      cinemaIDs: cinemas,
+      cinemaIDs: cinemaList,
       start: index,
       limit: limit
     });
 
     await page.goto(url);
+    //wait for the first session selector for max 5seconds before retrieving session info
+    await page.waitForSelector(
+      "#session-list > div.movie-container.list-view > ul > li > div.movie-list-detail.dynamic > div.cinemas > div > div"
+    , { timeout: 5000 }).catch(err=> {noSessionsCounter++});
+    
 
     //movies on the URL
     const movies = await moviesFn({ page });
-    
+    //console.log(movies);
 
     //Get the sessions for each @_movie
     const sessions = await movies.map(
-      async (movie) => {
-        const res = (await sessionsFn({ page: page }, movie.movieID)).map(session => {
-          return Object.assign({}, movie, session);
-        });
-       return res;
+      async movie => {
+        const res = (await sessionsFn({ page: page }, movie.movieID)).map(
+          session => {
+            return Object.assign({}, movie, session);
+          }
+        );
+        
+        return res;
       },
       { page }
     );
 
-    
     //Accumulate @_movieSessions
     sessionResults = R.concat(
       sessionResults,
@@ -67,7 +75,7 @@ module.exports = async (
     );
 
     //increment the while loop condition
-    index += R.min(cinemas.length - limit, limit);
+    index += R.min(cinemaList.length - limit, limit);
   }
   return sessionResults;
 };
