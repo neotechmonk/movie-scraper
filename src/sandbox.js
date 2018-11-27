1dee6b7289a90b30a4710a29b6c5843ff2c756b6
Theepan Thevathasan <nrotechmonk@gmail.com>
2018-11-27 15:31:36 +1100

    make it deploy

--- src/sandbox.js
+++
@@ -1,7 +0,0 @@
-import { default as writePretty } from "./write-prettily";
-const inputObject = {
-  outerProp: "outerProp",
-  innerObj: { innnerProp: "innnerProp" }
-};
-
-console.log(writePretty("output\\", "jsonFile.json", inputObject));
--- src/start.js
+++ src/start.js
@@ -2,13 +2,15 @@
 global.__basedir = __dirname;
 
 // import environmental variables from our variables.env file
-import {
-  isDevelopmentMode as isDev,
-  getEnvironmentSetting as setting
-} from "./helpers/helpers";
-const dbConnect = require("./database");
+// import {
+//   isDevelopmentMode as isDev,
+//   getEnvironmentSetting as setting
+// } from "./helpers/helpers";
+// const dbConnect = require("./database");
 
-//Connect to DB based on DEV vs PRO runing mode
-const dbConneciton = isDev()
-  ? dbConnect(setting("DB_URI_DEV"))
-  : dbConnect(setting("DB_URI_PROD"));
+// //Connect to DB based on DEV vs PRO runing mode
+// const dbConneciton = isDev()
+//   ? dbConnect(setting("DB_URI_DEV"))
+//   : dbConnect(setting("DB_URI_PROD"));
+
+  require("./scrape-bot")();
